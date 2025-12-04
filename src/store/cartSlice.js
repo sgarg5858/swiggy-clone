6250eb0d24238875b8013645964ebb9ba2cb2afc
Sanjay Garg <sgarg5858@gmail.com>
2025-12-04 22:29:55 +0000

    rm clogs

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -10,10 +10,8 @@ const cartSlice = createSlice({
     addItem: (state, { payload }) => {
       const items = [...state.items];
       const index = items?.findIndex((item) => payload.id === item.id);
-      console.log(index);
       if (index === -1) {
         items.push({ ...payload, count: 1 });
-        console.log(items);
       } else {
         items[index] = { ...items[index], count: items[index].count + 1 };
       }
@@ -29,7 +27,6 @@ const cartSlice = createSlice({
       if (itemCount > 1) {
         const updatedItem = { ...items[index], count: itemCount - 1 };
         items[index] = { ...updatedItem };
-        console.log(items);
         state.items = items;
         state.count = state.count - 1;
       } else {
@@ -44,7 +41,6 @@ const cartSlice = createSlice({
   },
 });
 
-console.log(cartSlice);
 export const { addItem, clearCart, removeItem } = cartSlice.actions;
 
 export default cartSlice.reducer;
